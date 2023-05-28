2026869653f1f3f8bf9ab26e116ea831c469a868
zhangjh <zhangjh@example.com>
2023-05-28 22:41:13 +0800

    add .vscode

---
+++ 2-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    string name;
+    string add;
+    cout <<"请输入你的姓名："<<endl;
+    cin >> name;
+    cout <<"请输入你的地址："<<endl;
+    cin>> add;
+    cout <<"你的姓名是："<< name <<",你的地址是："<< add <<"。"<<endl;
+    return 0;
+}
