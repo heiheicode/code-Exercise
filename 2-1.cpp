57c9fa0f302e255b549432a49cebab32f651f665
zhangw <zhangw@example.com>
2024-04-29 17:34:56 +0800

    add class

--- 2-1.cpp
+++ 2-1.cpp
@@ -10,5 +10,10 @@ int main()
     cout <<"请输入你的地址："<<endl;
     cin>> add;
     cout <<"你的姓名是："<< name <<",你的地址是："<< add <<"。"<<endl;
+    class Derived : public Base
+    public:
+    Derived(){};
+    ~Derived(){};
+    virtual void printClass(int num = 20) override {printf("Derived num is : %d\n", num);};
     return 0;
 }
